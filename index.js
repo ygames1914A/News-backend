d6ba06ce2dd8cdb1d28d361c898475cffc88ff80
oandygames.ygames <oandygames.ygames@gmail.com>
2026-06-05 16:25:18 +0300

    adding customized category and api key choser

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const app = express()
 app.use(cors())
 
 app.get("/", async (req,res) => {
-    const respones = await fetch("https://gnews.io/api/v4/top-headlines?category=general&lang=ar&country=eg&max=10&apikey=88bf36fa19025cef6c0c80cb59985cf3")
+    const {category , apikey} = req.query
+    const respones = await fetch(`https://gnews.io/api/v4/top-headlines?category=${category}&lang=ar&country=eg&max=10&apikey=${apikey}`)
     const data = await respones.json()
     res.send(data)
 })
